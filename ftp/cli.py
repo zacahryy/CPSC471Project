ea1a5ce3f46823e9267717f689c9b9df9412bdbb
ginnaaleee <131627008+ginnaaleee@users.noreply.github.com>
2024-05-08 15:05:57 -0700

    Update cli.py

--- ftp/cli.py
+++ ftp/cli.py
@@ -10,29 +10,7 @@ try:
     client.connect((serverNameT, serverPort)) #Connect to the server running on localhost
     
 while True:
-        command = input("ftp > ")
-        if(command=="GET"):
-            client.send(command.encode())
-            break
-        else:
-            print("Invaid comand")
-
-    #Data buffer
-    data = ""
-    while True:
-        received_data = client.recv(6).decode() #Receive the data sent by the client
-        if "<EOF>" in received_data:
-            break
-        data = data+received_data
-
-    with open(DOWNLOAD_FILE_PATH, "w") as file:
-        file.write(data)
-        print("Download successful")
-    
-
-except Exception as e:
-    print("Connection terminated!")
-    print(e)
+       
 
     if 
         clientSocket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
